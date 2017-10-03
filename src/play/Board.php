2c7c9b47d7360e235e092c9033c58f936be38680
Toan <thton@miners.utep.edu>
2017-10-02 23:11:03 -0600

    updated board

--- src/play/Board.php
+++ src/play/Board.php
@@ -1,19 +1,15 @@
 <?php
-
-public Class Board{
-
-	$size = 15*15;
-	$grid = array(array());
-
-	function __contruct(){
-		for ($i=0 ; $i < 15 ; $i++ ) { 
-			for ($j=0; $j < 15; $j++) { 
-				$grid[i][j] = 0;
+class Board {
+	public $grid = array (
+			array () 
+	);
+	function __contruct() {
+		for($i = 0; $i < 15; $i ++) {
+			for($j = 0; $j < 15; $j ++) {
+				$grid [i] [j] = 0;
 			}
 		}
 	}
 }
-
-
 
 ?>

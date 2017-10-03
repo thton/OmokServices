02f3d8fb72372c59ab621c591304e80ec13b39ce
thton <thton@miners.utep.edu>
2017-10-02 19:50:16 -0600

    got responses working for new/index.php

--- src/new/index.php
+++ src/new/index.php
@@ -1,3 +1,28 @@
 <?php
 
+$Strats = $_GET['strategy'];
+
+if(!$Strats){
+	$response = "false";
+	$reason = "Strategy not specified";
+}
+
+elseif($Strats == "Smart" || $Strats == "Random"){
+	$response = "true";
+	$pid = uniqid();
+}
+
+else{
+	$response = "false";
+	$reason = "Unknown strategy";
+}
+
+if ($response == "false") {
+	$output = array("response"=>$response,"reason"=>$reason);
+	echo json_encode($output);
+}
+else{
+	$output = array("response"=>$response,"pid"=>$pid);
+	echo json_encode($output);
+}
 ?>
